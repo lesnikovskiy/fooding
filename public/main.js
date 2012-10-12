d17cbbde0ab5721fab1ca2c77bef2c8fc191b434
lesnikovskiy <lesnikovski@gmail.com>
2012-10-12 14:19:34 +0300

    enable to save and display products

--- public/main.js
+++ public/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
-	// text will be added here
+	$('#new-product-form').submit(function() {
+		$.ajax({
+			type: 'POST',
+			data: $(this).serialize(),
+			url: $(this).attr('action'),
+			success: function(res) {
+				if (res.ok) {
+					alert('saved');
+				}
+				
+				if (res.error) {
+					alert('error occurred');
+				}
+			}
+		});
+	
+		return false;
+	});
 });
