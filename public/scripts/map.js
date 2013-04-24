37480e5896ade85868b3b4ab4a34a59001fc43b6
lesnikovskiy <lesnikovski@gmail.com>
2013-04-24 17:56:40 +0300

    finally added working callback for the map

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -4,8 +4,52 @@
 	var initLngCoord = 30.598297119140625;
 	var initZoom = 12;
 	
+	$el = function (el) {
+		var elem = document.createElement(el);
+		
+		elem.setid = function(id) {
+			elem.id = id;
+			return this;
+		};
+		
+		elem.css = function(css) {
+			for (var i in css) 
+				elem.style[i] = css[i];
+				
+			return this;
+		};
+		
+		elem.attr = function(attr) {
+			for (var i in attr)
+				elem[i] = attr[i];
+				
+			return this;
+		};
+		
+		elem.text = function(txt) {
+			elem.appendChild(document.createTextNode(txt));
+			return this;
+		}
+		
+		elem.append = function(what) {
+			(elem || document).appendChild(what);			
+			return this;
+		};
+		
+		elem.before = function(sibling) {
+			sibling.parentNode.insertBefore(elem, sibling);
+			return this;
+		};
+		
+		elem.addEvent = function(event, callback) {
+			elem['on' + event] = callback;
+			return this;
+		}
+		
+		return elem;
+	};
+	
 	var submitMarker = function() {
-		debugger;
 		var data = JSON.stringify({
 				lat: $('#lat').val(),
 				lng: $('#lng').val(),
@@ -31,22 +75,68 @@
 	
 		return false;
 	};
-
-	window.onload = function() {
+	
+	window.onload = function() {	
+		var Coord = function(coords) {
+			debugger;
+			this.id = ko.observable(coords.id);
+			this.rev = ko.observable(coords.rev);
+			this.lat = ko.observable(coords.lat);
+			this.lng = ko.observable(coords.lng);
+			this.title = ko.observable(coords.title);
+			this.desc = ko.observable(coords.desc);
+		};
 		
+		var CoordViewModel = function() {
+			var self = this;
+			
+			self.lng = ko.observable();
+			self.lat = ko.observable();
+			self.title = ko.observable();
+			self.desc = ko.observable();
+			
+			self.coords = ko.observableArray([]);
+			self.addCoord = function() {
+				var coord = new Coord({
+					lng: self.lng(),
+					lat: self.lat(),
+					title: self.title(),
+					desc: self.desc()
+				});
+				
+				$.ajax({
+					type: 'POST',
+					url: '/api/map',
+					data: ko.toJS(coord),
+					success: function(data) {
+						console.log(data);
+					}
+				});
+				
+				return false;
+			};
+		};
 	
 		var createMarkerForm = function(lat, lng) {
-			var html = '<form id="marker-form" onsubmit="return submitMarker();">';
-			html += '<input type="hidden" value="' + lat + '" id="lat" />';
-			html += '<input type="hidden" value="' + lng + '" id="lng" />';
-			html += '<label for="title">Title:</label>'
-			html += '<input type="text" id="title" />';
-			html += '<label for="desc">Description:</label>'
-			html += '<textarea id="desc"></textarea>';
-			html += '<input type="submit" value="submit" id="submit-marker" />';
-			html += '</form>';
+			var hiddenLat = $el('input').attr({value: lat, type: 'hidden', id: 'lat'});		
+			var hiddenLng = $el('input').attr({value: lng, type: 'hidden', id: 'lng'});
+			var label = $el('label').attr({htmlFor: 'title'}).text('Title:');			
+			var title = $el('input').attr({type: 'text', id: 'title'});			
+			var descLabel = $el('label').attr({htmlFor: 'desc'}).text('Description:');			
+			var textarea = $el('textarea').setid('desc');			
+			var submit = $el('input').attr({type: 'submit', value: 'submit', id: 'submit-marker'});
 			
-			return html;
+			var form = $el('form').attr({method: 'post', action: '/api/map', id: 'marker-form'})
+							.addEvent('submit', submitMarker)
+							.append(hiddenLat)
+							.append(hiddenLng)
+							.append(label)
+							.append(title)
+							.append(descLabel)
+							.append(textarea)
+							.append(submit);
+			
+			return form;
 		};
 
 		if (!GBrowserIsCompatible()) {
@@ -60,8 +150,10 @@
 		map.setCenter(new GLatLng(initLatCoord, initLngCoord), initZoom);
 		
 		GEvent.addListener(map, 'click', function(overlay, latlng) {
-			if (latlng)
+			if (latlng) {
 				map.openInfoWindow(latlng, createMarkerForm(latlng.lat(), latlng.lng()));
+				var form = document.getElementById('marker-form');
+			}
 		});
 	};
 
